2d9c1a1715df680dcc2266d0bbdb7ce3081bfde8
Alexey Pochtovik <alexeydp92dp@gmail.com>
2022-11-16 17:03:00 +0200

    add port

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await app.listen(3007);
+  await app.listen(process.env.PORT || 3007);
 }
 bootstrap();
